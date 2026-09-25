c7b74e347ec1745f5509c07359a72656c16879f6
mburzhinsky-hub <m.burzhinsky@visuals.ru>
2026-09-25 09:19:59 +0300

    chore: align GOTHAM asset version

--- app.js
+++ app.js
@@ -694,7 +694,7 @@ function initCustomControls(){
 }
 
 renderPresets();applyLook();syncGrid(true);syncHist(true);initMechanicalDials();initCustomControls();
-if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=20260925-void-gotham-1',{updateViaCache:'none'}).catch(()=>{}));
+if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=20260925-void-gotham-2',{updateViaCache:'none'}).catch(()=>{}));
 
 const presetMenu=$('presetMenuButton');
 if(presetMenu) presetMenu.onclick=()=>{
