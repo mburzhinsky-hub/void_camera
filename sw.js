d5a4dcc5e831a5fe1a1217390ce80854286a6eda
mburzhinsky-hub <m.burzhinsky@visuals.ru>
2026-09-25 08:19:10 +0300

    fix: cache expanded preset library

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='void-camera-artifactfix-20260925-1';
+const CACHE='void-camera-presets2-20260925-1';
 const ASSETS=[
   './',
   './index.html',
-  './styles.css?v=20260925-void-artifactfix-1',
-  './sound-engine.js?v=20260925-void-artifactfix-1',
-  './mechanical-dial.js?v=20260925-void-artifactfix-1',
-  './film-engine.js?v=20260925-void-artifactfix-1',
-  './app.js?v=20260925-void-artifactfix-1',
+  './styles.css?v=20260925-void-presets2-1',
+  './sound-engine.js?v=20260925-void-presets2-1',
+  './mechanical-dial.js?v=20260925-void-presets2-1',
+  './film-engine.js?v=20260925-void-presets2-1',
+  './app.js?v=20260925-void-presets2-1',
   './manifest.webmanifest',
   './icon.svg',
   './assets/presets/classic_city.svg',
